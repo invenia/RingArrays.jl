68173f501f426e5a491d30ff9553323169dd180f
Samuel Massinon <samuel.massinon@invenia.ca>
2016-01-28 13:40:26 -0600

    Added BaseTestNext for 0.4

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using RingArrays
-using Base.Test
 using FactCheck
+
+if VERSION >= v"0.5-"
+    using Base.Test
+else
+    using BaseTestNext
+    const Test = BaseTestNext
+end
 
 include("RingArrays.jl")
 
